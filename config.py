8ee8a8be965b45b93459666b94f6dcec57d1477d
MikasaEureka <37149207+MikasaEureka@users.noreply.github.com>
2023-05-30 09:36:00 +0000

    add env

--- config.py
+++ config.py
@@ -1,36 +1,14 @@
+import os
 config = {
     "multi": [
         {
-            "account": "xxx",
-            "password": "xxx",
-            # "push": {
-            #     "type": "pushplus",
-            #     "key": "xxx",
-            # },
+            "account": os.environ['tyyp_user'],
+            "password": os.environ['tyyp_pwd'],
         },
-        # {
-        #     "account": "xxx",
-        #     "password": "xxx",
-        #     # "push": [
-        #     #     # 以数组的形式填写, 则会向多个服务推送消息
-        #     #     {
-        #     #         "type": "pushplus",
-        #     #         "key": "xxx",
-        #     #     },
-        #     #     {
-        #     #         "type": "workWechat",
-        #     #         "key": {
-        #     #             "agentid": 1000002,
-        #     #             "corpSecret": "xxx",
-        #     #             "corpid": "xxx",
-        #     #         },
-        #     #     },
-        #     # ],
-        # },
     ],
     "push": {
         # 合并发送消息, 只合并未单独配置 push 的账号
         "type": "pushplus",
-        "key": "xxx",
+        "key": os.environ['PUSHPLUS_TOKEN'],
     },
 }
